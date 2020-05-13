735d86fedf956309c760b721b471340fb355f2e7
shieda4 <shiedaiz10@gmail.com>
2020-05-13 15:52:44 +0800

    Data Storage Added

---
+++ data_store.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Store(object):
+    def __init__(self):
+        self.player1 = []
+        self.player2 = []
+        self.merged = []
+        pass
+
+    def update_store(self, data, player1=False, player2=False):
+        state, policy, value = data[0], data[1], data[2]
+        if player1:
+            self.player1.append([state, policy, value])
+        if player2:
+            self.player2.append([state, policy, value])
+
+    def save_store(self, filename):
+        self.merged.append(self.player1)
+        self.merged.append(self.player2)
+        np.save(filename + ".npy", self.merged)
+
+    def load_store(self, filename):
+        temp = np.load(filename + 'npy')
+        self.merged = temp.tolist()
+        self.player1 = self.merged[0]
+        self.player2 = self.merged[1]
+
+    def get_store(self):
+        self.merged.clear()
+        self.merged.append(self.player1)
+        self.merged.append(self.player2)
+        return self.merged
